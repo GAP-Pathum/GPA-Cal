ea5fce9c4d970800fb16c2fc3674dd1d59ff9034
Pasindu Pathum <pathumpasindu41@gmail.com>
2026-06-05 17:34:37 +0530

    enable cloud

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js — DO NOT commit this file (see .gitignore)
+var firebaseConfig = {
+  apiKey: "AIzaSyDFkS0H6vczUMKTbUqtMqsHkAZgGTTkWzw",
+  authDomain: "gpa-cal-40813.firebaseapp.com",
+  projectId: "gpa-cal-40813",
+  storageBucket: "gpa-cal-40813.firebasestorage.app",
+  messagingSenderId: "46906168814",
+  appId: "1:46906168814:web:d2ed7b0bb65352a503aaff",
+  measurementId: "G-W91PV3G390"
+};
